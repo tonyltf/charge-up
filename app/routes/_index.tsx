12d37d1b2242bec0d6e2434fc2ad3eee0ae97cf5
Tony Li <tingfung.tony@gmail.com>
2024-03-09 00:04:24 +0800

    🔧 (pre-commit.yml): update pre-commit workflow to use the latest versions of actions/checkout, actions/setup-python, and pre-commit/action ♻️ (routes/_index.tsx): refactor the return statement in the Index component to remove unnecessary line breaks and improve code readability

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,11 +11,5 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function Index() {
   const { todos } = useLoaderData<typeof loader>();
-  return (
-    <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
-      ))}
-    </ul>
-  );
+  return <ul>{todos?.map((todo) => <li key={todo.id}>{todo.name}</li>)}</ul>;
 }
